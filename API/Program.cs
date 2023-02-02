fc9247852249b9ca515bc7e6605c6c22688ed0d8
Maha Fayez Mohaisen <maha.mohasen.se@gmail.com>
2023-02-02 16:01:11 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<DataContext>(opt =>{
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +28,7 @@ var app = builder.Build();
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
-
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
